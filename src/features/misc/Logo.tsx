dbfdd2e7f04bb1c087dfe552ab8c1c8406ed9e29
Michael Lei <michaelleie0ca@gmail.com>
2023-07-18 12:19:30 -0700

    Update logo size

--- src/features/misc/Logo.tsx
+++ src/features/misc/Logo.tsx
@@ -5,7 +5,7 @@ import routes from "../../routes";
 function Logo() {
   return (
     <Link to={routes.home}>
-      <h1 className="flex items-center gap-2 text-2xl font-bold tracking-tighter xl:gap-5 xl:text-5xl">
+      <h1 className="flex items-center gap-2 text-3xl font-bold tracking-tighter xl:gap-5 xl:text-5xl">
         <CameraIcon className="h-8 w-8 xl:h-12 xl:w-12" /> Snapshot
       </h1>
     </Link>
