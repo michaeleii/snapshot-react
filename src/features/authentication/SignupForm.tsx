4459c796a59272644d79f94df2311226f9c66f9e
Michael Lei <michaelleie0ca@gmail.com>
2023-07-18 15:22:59 -0700

    Add react hook form

--- src/features/authentication/SignupForm.tsx
+++ src/features/authentication/SignupForm.tsx
@@ -1,10 +1,39 @@
+import { z } from "zod";
+
 import Button from "../misc/Button";
 import Logo from "../misc/Logo";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-function LoginForm() {
+const schema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 8 characters long" }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+function SignupForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
   return (
     <form
       action=""
+      onSubmit={handleSubmit(onSubmit)}
       className="flex h-screen  flex-col justify-center gap-5  bg-white p-10 xl:min-h-screen xl:justify-center xl:px-20"
     >
       <div className="mx-auto mb-5">
@@ -16,7 +45,7 @@ function LoginForm() {
       <div>
         <input
           type="text"
-          name="username"
+          {...register("username")}
           placeholder="Username"
           className="w-full rounded-full border-2 px-5 py-3 placeholder:text-gray-300"
         />
@@ -24,7 +53,7 @@ function LoginForm() {
       <div>
         <input
           type="email"
-          name="email"
+          {...register("email")}
           placeholder="Email Address"
           className="w-full rounded-full border-2 px-5 py-3 placeholder:text-gray-300"
         />
@@ -32,7 +61,7 @@ function LoginForm() {
       <div>
         <input
           type="text"
-          name="password"
+          {...register("password")}
           placeholder="Password"
           className="w-full rounded-full border-2 px-5 py-3 placeholder:text-gray-300"
         />
@@ -43,4 +72,4 @@ function LoginForm() {
     </form>
   );
 }
-export default LoginForm;
+export default SignupForm;
