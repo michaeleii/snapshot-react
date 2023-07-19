21fceb887404d15b0f8ea49db16af41b3b6c4c8f
Michael Lei <michaelleie0ca@gmail.com>
2023-07-18 20:27:49 -0700

    Remove unused

--- src/features/auth/useLogout.ts
+++ src/features/auth/useLogout.ts
@@ -1,6 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout } from "../../services/apiAuth";
-import { useNavigate } from "react-router-dom";
 
 export function useLogout() {
   const queryClient = useQueryClient();
